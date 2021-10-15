4a21e9bdc2c01347f8967ef4378ec0f2a9ed63b5
dandrok <patryk.stusik@gmail.com>
2021-10-15 18:24:15 +0200

    super secret pass gen

---
+++ Super_Secret_Password_Generator.js
@@ -0,0 +1,12 @@
+// Sys Admins are always on your case to improve the strength of your passwords. You could really use a handy function to make your passwords completely un-hackable.
+
+// Use the super secret characters in the superSecretChars variable to replace the matching characters in your totally insecure password and make it un-hackable.
+
+// eg. replace all 'a's with '@'s. Make sure you get the upper case characters too just in case the user wants to SHOUT their password at you.
+
+// ANSWER:
+
+const createSSP = (p) => {
+  const ch = { a: '@', s: '$', o: '0', h: '5', x: '*' }
+  return p.replace(/[asohx]/gi, (m) => ch[m.toLowerCase()])
+}
