9138ef4556bf7e3917b8c68519c73a42433b13e8
dandrok <patryk.stusik@gmail.com>
2021-10-24 20:48:41 +0200

    price of mangoes

---
+++ Price_of_Mangoes.js
@@ -0,0 +1,9 @@
+// There's a "3 for 2" (or "2+1" if you like) offer on mangoes. For a given quantity and price (per mango), calculate the total cost of the mangoes.
+
+// Examples
+// mango(3, 3) ==> 6    # 2 mangoes for 3 = 6; +1 mango for free
+// mango(9, 5) ==> 30   # 6 mangoes for 5 = 30; +3 mangoes for free
+
+// ANSWER:
+// q = quantity, p = price
+const mango = (q, p) => (q - Math.floor(q / 3)) * p
