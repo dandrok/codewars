7b923d3508fe1567e5652ec705539405620772a6
dandrok <patryk.stusik@gmail.com>
2021-11-06 23:41:48 +0100

    greet me

---
+++ Greet Me.js
@@ -0,0 +1,10 @@
+/* 
+Write a method that takes one argument as name and then greets that name, capitalized and ends with an exclamation point.
+
+Example:
+
+"riley" --> "Hello Riley!"
+"JACK"  --> "Hello Jack!"
+ */
+const greet = (n) =>
+  `Hello ${n.charAt(0).toUpperCase() + n.toLowerCase().slice(1)}!`
