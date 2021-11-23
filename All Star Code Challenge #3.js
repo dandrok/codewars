631e26d289213e1d16f0ae8fbcb3c878a2aca112
dandrok <patryk.stusik@gmail.com>
2021-11-23 22:55:12 +0100

    replace

---
+++ All Star Code Challenge #3.js
@@ -0,0 +1,15 @@
+/* 
+This Kata is intended as a small challenge for my students
+
+Create a function, called removeVowels (or remove_vowels), that takes a string argument and returns that same string with all vowels removed (vowels are "a", "e", "i", "o", "u").
+
+removeVowels("drake") // => "drk"
+removeVowels("aeiou") // => ""
+ */
+
+const removeVowels = (s) =>
+  [...s].map((a) => a.replace(/a|e|i|o|u/gi, '')).join('')
+
+//or
+
+const removeVowels = (s) => str.replace(/[aeiou]/gi, '')
