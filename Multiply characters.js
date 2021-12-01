6e32b30c4fd08d2989dd0991a0990074db47d2cf
dandrok <patryk.stusik@gmail.com>
2021-12-01 22:31:39 +0100

    multiply characters

---
+++ Multiply characters.js
@@ -0,0 +1,10 @@
+/* 
+Here we have a function that help us spam our hearty laughter. But is not working! I need you to find out why...
+
+Expected results:
+
+spam(1);  // hue
+spam(6);  // huehuehuehuehuehue
+spam(14); // huehuehuehuehuehuehuehuehuehuehuehuehuehue
+ */
+const spam = (n) => 'hue'.repeat(n)
