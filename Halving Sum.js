f18e8a704670f2a1b8f0866c3135b1cfb11800c1
dandrok <patryk.stusik@gmail.com>
2021-12-13 23:01:32 +0100

    halving sum

---
+++ Halving Sum.js
@@ -0,0 +1,14 @@
+/* 
+Task
+Given a positive integer n, calculate the following sum:
+
+n + n/2 + n/4 + n/8 + ...
+All elements of the sum are the results of integer division.
+
+Example
+25  =>  25 + 12 + 6 + 3 + 1 = 47
+ */
+const halvingSum = (n) => {
+  if (n === 1) return 1
+  return n + halvingSum(Math.floor(n / 2))
+}
