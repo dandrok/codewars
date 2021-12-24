6dd9306effd490d8025eb244be11aea3a6c4cb7b
dandrok <patryk.stusik@gmail.com>
2021-12-24 19:15:57 +0100

    changes

---
+++ Contamination #1 -String-.js
@@ -0,0 +1,28 @@
+/* 
+An AI has infected a text with a character!!
+
+This text is now fully mutated to this character.
+
+If the text or the character are empty, return an empty string.
+There will never be a case when both are empty as nothing is going on!!
+
+Note: The character is a string of length 1 or an empty string.
+
+Example
+text before = "abc"
+character   = "z"
+text after  = "zzz"
+ */
+
+const contamination = (t, c) => {
+  return c.repeat(t.length)
+}
+
+// OR
+
+const contamination = (text, char) => {
+  return text
+    .split('')
+    .map((a) => a.replace(a, char))
+    .join('')
+}
