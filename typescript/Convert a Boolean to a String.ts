485f59a16c04396c94c0c59dda1bde3f589ffcdd
dandrok <patryk.stusik@gmail.com>
2022-01-05 23:07:12 +0100

    convert boolean to string

---
+++ typescript/Convert a Boolean to a String.ts
@@ -0,0 +1,7 @@
+/* 
+Implement a function which convert the given boolean value into its string representation.
+
+Note: Only valid inputs will be given.
+ */
+
+export const booleanToString = (b: boolean): string => b.toString()
