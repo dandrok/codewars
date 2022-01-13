a8d3b632f733b252d5fe03eacb22494eb4bce045
dandrok <patryk.stusik@gmail.com>
2022-01-13 21:55:10 +0100

    returning strings ts

---
+++ typescript/ Returning Strings.ts
@@ -0,0 +1,8 @@
+/* 
+Make a function that will return a greeting statement that uses an input; your program should return, "Hello, <name> how are you doing today?".
+
+[Make sure you type the exact thing I wrote or the program may not execute properly]
+ */
+
+export const greet = (name: string): string =>
+  `Hello, ${name} how are you doing today?`
