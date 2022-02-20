53360d84af48300634cbcc379483f77fb62485d4
dandrok <patryk.stusik@gmail.com>
2022-02-20 18:13:17 +0100

    two to one ts

---
+++ typescript/Two to One.ts
@@ -0,0 +1,17 @@
+/* 
+Take 2 strings s1 and s2 including only letters from ato z. Return a new sorted string, the longest possible, containing distinct letters - each taken only once - coming from s1 or s2.
+
+Examples:
+a = "xyaabbbccccdefww"
+b = "xxxxyyyyabklmopq"
+longest(a, b) -> "abcdefklmopqwxy"
+
+a = "abcdefghijklmnopqrstuvwxyz"
+longest(a, a) -> "abcdefghijklmnopqrstuvwxyz"
+ */
+
+export class G964 {
+  public static longest = (s1: string, s2: string): string => {
+    return [...new Set(s1.concat(s2))].sort().join('')
+  }
+}
