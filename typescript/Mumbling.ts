b78a510baf5df6d5e8d27dcc4ef5e7eba06183a7
dandrok <patryk.stusik@gmail.com>
2022-02-22 22:03:22 +0100

    mumbling ts

---
+++ typescript/Mumbling.ts
@@ -0,0 +1,16 @@
+/* 
+This time no story, no theory. The examples below show you how to write function accum:
+
+Examples:
+accum("abcd") -> "A-Bb-Ccc-Dddd"
+accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+accum("cwAt") -> "C-Ww-Aaa-Tttt"
+The parameter of accum is a string which includes only letters from a..z and A..Z.
+ */
+
+export const accum = (s: string): string => {
+  return s
+    .split('')
+    .map((el, i) => el.charAt(0).toUpperCase() + el.toLowerCase().repeat(i))
+    .join('-')
+}
