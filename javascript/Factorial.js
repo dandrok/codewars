b60a54f0577fc6f9b1b2d4ede25a2c5dfcaf6620
dandrok <patryk.stusik@gmail.com>
2022-02-26 21:42:16 +0100

    factorial js edit

--- javascript/Factorial.js
+++ javascript/Factorial.js
@@ -6,6 +6,6 @@ https://en.wikipedia.org/wiki/Factorial
 
 function factorial(n) {
   let arr = [...Array(n + 1).keys()]
-  let remLast = arr.shift()
+  arr.shift()
   return arr.reduce((a, b) => a * b, 1)
 }
