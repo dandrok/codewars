dfe4696b40cdc41b9f64f8fd492cff3272fa1b52
dandrok <patryk.stusik@gmail.com>
2022-08-25 06:46:58 +0200

    number pairs js

---
+++ javascript/Number Pairs.js
@@ -0,0 +1,14 @@
+/* 
+In this Kata the aim is to compare each pair of integers from 2 arrays, and return a new array of large numbers.
+
+Note: Both arrays have the same dimensions.
+
+Example:
+
+let arr1 = [13, 64, 15, 17, 88];
+let arr2 = [23, 14, 53, 17, 80];
+getLargerNumbers(arr1, arr2); // Returns [23, 64, 53, 17, 88]
+ */
+
+const getLargerNumbers = (a, b) =>
+  a.map((el, i, arr) => (arr[i] > b[i] ? arr[i] : b[i]))
