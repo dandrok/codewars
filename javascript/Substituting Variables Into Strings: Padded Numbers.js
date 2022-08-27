d0b50e401d2d9befde63e45d702318007fdcfe93
dandrok <patryk.stusik@gmail.com>
2022-08-27 21:25:11 +0200

    substituting veriables into strings padded numbers js

---
+++ javascript/Substituting Variables Into Strings: Padded Numbers.js
@@ -0,0 +1,10 @@
+/* 
+Complete the solution so that it returns a formatted string. The return value should equal "Value is VALUE" where value is a 5 digit padded number.
+
+Example:
+
+solution(5) // should return "Value is 00005"
+ */
+
+const solution = (value) =>
+  `Value is ${'0'.repeat(5 - String(value).length)}${value}`
