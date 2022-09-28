f1db8908b700e660b32a7938a4cc252a49695aa8
dandrok <patryk.stusik@gmail.com>
2022-09-28 19:47:03 +0200

    sort arrays 1 js

---
+++ javascript/Sort arrays - 1.js
@@ -0,0 +1,5 @@
+/* 
+Just a simple sorting usage. Create a function that returns the elements of the input-array / list sorted in lexicographical order.
+ */
+
+export const sortMe = (names: string[]): string[] => names.sort()
