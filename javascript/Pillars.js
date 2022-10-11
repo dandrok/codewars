4df40ac6cb42a5aa4870f9f48190f6b3383bed40
dandrok <patryk.stusik@gmail.com>
2022-10-11 07:05:11 +0200

    pillars js

---
+++ javascript/Pillars.js
@@ -0,0 +1,18 @@
+/* 
+There are pillars near the road.
+The distance between the pillars is the same and the width of the pillars is the same.
+
+Your function accepts three arguments:
+number of pillars (≥ 1);
+distance between pillars (10 - 30 meters);
+width of the pillar (10 - 50 centimeters).
+
+Calculate the distance between the first and the last pillar in centimeters (without the width of the first and last pillar).
+ */
+
+function pillars(numPill, dist, width) {
+  if (numPill == 1) return 0
+  dist = dist * 100
+  const oneP = width + dist
+  return oneP * numPill - dist - width * 2
+}
