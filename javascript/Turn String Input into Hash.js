f2dbf30602fe09c9a5b74cad0e005abf9e326303
dandrok <patryk.stusik@gmail.com>
2022-10-30 07:59:24 +0100

    turn string input into hash js

---
+++ javascript/Turn String Input into Hash.js
@@ -0,0 +1,18 @@
+/* 
+Please write a function that will take a string as input and return a hash.
+The string will be formatted as such.
+The key will always be a symbol and the value will always be an integer.
+
+"a=1, b=2, c=3, d=4"
+This string should return a hash that looks like
+
+{ 'a': 1, 'b': 2, 'c': 3, 'd': 4}
+ */
+
+const strToHash = (str) => {
+  if (str.length < 1) return {}
+  let obj = {}
+  const arr = str.split(', ').map((el) => el.split('='))
+  arr.map((ar, i) => (obj[arr[i][0]] = Number(arr[i][1])))
+  return obj
+}
