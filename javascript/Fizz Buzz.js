2909762ee539d61f301f3ab3c304c9f627b2af52
dandrok <patryk.stusik@gmail.com>
2022-11-16 08:08:22 +0100

    fizz buzz 2 js

--- javascript/Fizz Buzz.js
+++ javascript/Fizz Buzz.js
@@ -25,3 +25,16 @@ const fizzbuzz = (n) => {
       : num
   )
 }
+
+const fizzbuzz2 = (n) => {
+  return Array.from({ length: n }, (_, i) => {
+    let num = i + 1
+    return num % 3 == 0 && num % 5 == 0
+      ? 'FizzBuzz'
+      : num % 3 == 0
+      ? 'Fizz'
+      : num % 5 == 0
+      ? 'Buzz'
+      : num
+  })
+}
