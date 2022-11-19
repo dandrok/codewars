b81e028a051fc63c86bfff6076db6f3a245d7f55
dandrok <patryk.stusik@gmail.com>
2022-11-19 07:19:20 +0100

    edit crash override js

--- javascript/Crash Override.js
+++ javascript/Crash Override.js
@@ -23,66 +23,9 @@ aliasGen('123abc', 'Petrovic') === 'Your name must start with a letter from A - 
 Happy hacking!
  */
 
-let fakeFirstName = {
-  A: 'Alpha',
-  B: 'Beta',
-  C: 'Cache',
-  D: 'Data',
-  E: 'Energy',
-  F: 'Function',
-  G: 'Glitch',
-  H: 'Half-life',
-  I: 'Ice',
-  J: 'Java',
-  K: 'Keystroke',
-  L: 'Logic',
-  M: 'Malware',
-  N: 'Nagware',
-  O: 'OS',
-  P: 'Phishing',
-  Q: 'Quantum',
-  R: 'RAD',
-  S: 'Strike',
-  T: 'Trojan',
-  U: 'Ultraviolet',
-  V: 'Vanilla',
-  W: 'WiFi',
-  X: 'Xerox',
-  Y: 'Y',
-  Z: 'Zero',
-}
-let fakeSecondName = {
-  A: 'Analogue',
-  B: 'Bomb',
-  C: 'Catalyst',
-  D: 'Discharge',
-  E: 'Electron',
-  F: 'Faraday',
-  G: 'Gig',
-  H: 'Hacker',
-  I: 'IP',
-  J: 'Jabber',
-  K: 'Killer',
-  L: 'Lazer',
-  M: 'Mike',
-  N: 'n00b',
-  O: 'Overclock',
-  P: 'Payload',
-  Q: 'Quark',
-  R: 'Roy',
-  S: 'Spy',
-  T: 'T-Rex',
-  U: 'Unit',
-  V: 'Virus',
-  W: 'Worm',
-  X: 'X',
-  Y: 'Yob',
-  Z: 'Zombie',
-}
-
-function aliasGen(firstName, surname) {
-  const first = fakeFirstName[firstName[0].toUpperCase()]
-  const second = fakeSecondName[surname[0].toUpperCase()]
+const aliasGen = (f, s) => {
+  const first = firstName[f[0].toUpperCase()]
+  const second = surname[s[0].toUpperCase()]
 
   return first === undefined || second === undefined
     ? 'Your name must start with a letter from A - Z.'
