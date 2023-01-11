83b5a57ee92155fe0680cea051a0c773c88fe219
dandrok <46764643+dandrok@users.noreply.github.com>
2023-01-11 07:25:58 +0000

    js golfing #4: math.random js

---
+++ javascript/JS Golfing #4: Math.random.js
@@ -0,0 +1,14 @@
+/* 
+   JS Golfing Series  
+
+Task:
+Return 'random' Numbers ( 0 <= n < 100 ) with no more than 18 characters.
+
+    Math.random is forbidden
+    Numbers have to be Integers!
+    The usage of global variables or arrays is not allowed
+    If the test do not pass within the first time, try to submit again.
+    (at least 50% of the numbers have to be unique) ;-)
+ */
+
+//x=()=>new Date%100
