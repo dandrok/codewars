af715b827dfa6ff5dd6e9c0a1828153319ce6f92
dandrok <46764643+dandrok@users.noreply.github.com>
2023-01-13 16:11:41 +0000

    js golfing #8: is is uppercase? js

---
+++ javascript/JS Golfing #8: Is it Uppercase?.js
@@ -0,0 +1,20 @@
+/*
+  
+ JS Golfing Series 
+ 
+
+Task:
+
+Check if the input ( one letter ) is Uppercase with no more than 9 characters.
+
+No need to check for invalid input.
+Only letters will be tested.
+Return true if Uppercase and false if Lowercase.
+ */
+
+//In JavaScript, Strings are compared lexicographically (i.e. based on their unicode values),
+//and the string 'L' is lexicographically greater than the empty object '{}' and the string 'o' is
+//lexicographically smaller than the empty object '{}',
+//so the function will return true for exmple 'L' and false for example'o'
+
+//f=s=>s<{}
