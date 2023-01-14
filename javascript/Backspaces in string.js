659a1f7317630412ff0d04f2dce35b2f3b052b10
dandrok <46764643+dandrok@users.noreply.github.com>
2023-01-14 08:10:00 +0000

    backspaces in string js

---
+++ javascript/Backspaces in string.js
@@ -0,0 +1,19 @@
+/* 
+Assume "#" is like a backspace in string.
+This means that string "a#bc#d" actually is "bd"
+
+Your task is to process a string with "#" symbols.
+Examples
+
+"abc#d##c"      ==>  "ac"
+"abc##d######"  ==>  ""
+"#######"       ==>  ""
+""              ==>  ""
+
+ */
+
+function cleanString(s) {
+  let stack = []
+  ;[...s].forEach((el) => (el === '#' ? stack.pop() : stack.push(el)))
+  return stack.join('')
+}
