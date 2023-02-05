b550ac59773b2c531ee95f180c245964c9d91927
dandrok <46764643+dandrok@users.noreply.github.com>
2023-02-05 07:07:00 +0000

    reversed strings ts

---
+++ typescript/Reversed Strings.ts
@@ -0,0 +1,8 @@
+/* 
+Complete the solution so that it reverses the string passed into it.
+
+'world'  =>  'dlrow'
+'word'   =>  'drow'
+ */
+
+export const solution = (str: string): string => [...str].reverse().join('')
