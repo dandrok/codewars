d1b89240bb88b10f806a580754fbff92191a3eec
dandrok <46764643+dandrok@users.noreply.github.com>
2023-02-16 05:50:25 +0000

    asperand pixel counting js

---
+++ javascript/Asperand pixel counting.js
@@ -0,0 +1,27 @@
+/*
+Description
+
+You can paint an asperand by pixels in three steps:
+
+    First you paint the inner square, with a side of k.
+    Then you need to paint one pixel, that's laying diagonally relative to the inner square that you just painted
+    ( the bottom-right corner of the inner square is touching the top-left corner of the pixel ). Let's call it the "bridge".
+    Finally, you will need to paint the outer shape, connected diagonally to the "bridge" ( see the picture for more information ).
+
+Here are some examples of this:
+
+Examples for 0<k<5
+
+Your task is to find the number of pixels that need to be painted, for different k:
+
+k = 1 => 11
+k = 2 => 18
+k = 3 => 26
+k = 4 => 34
+
+# Limitations are 1 ≤ k ≤ 1e9
+
+The idea for this kata was taken from the Ukrainian Informatics Olympiad 2023.
+ */
+
+const countPixels = (k) => (k === 1 ? 11 : k === 2 ? 11 + 7 : (k - 2) * 8 + 18)
