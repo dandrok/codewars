1d899216b5b7d252f7c115f1df4ec18563ee779d
dandrok <46764643+dandrok@users.noreply.github.com>
2023-02-20 07:26:40 +0000

    format words into a sentence js

---
+++ javascript/Format words into a sentence.js
@@ -0,0 +1,25 @@
+/* 
+Complete the method so that it formats the words into a single comma separated value.
+The last word should be separated by the word 'and' instead of a comma.
+The method takes in an array of strings and returns a single formatted string.
+
+Note:
+
+    Empty string values should be ignored.
+    Empty arrays or null/nil/None values being passed into the method should result in an empty string being returned.
+
+Example: (Input --> output)
+
+['ninja', 'samurai', 'ronin'] --> "ninja, samurai and ronin"
+['ninja', '', 'ronin'] --> "ninja and ronin"
+[] -->""
+ */
+
+const formatWords = (words) => {
+  words ? (words = words.filter(Boolean)) : words
+  return !words
+    ? ''
+    : words.length > 1
+    ? `${words.slice(0, -1).join(', ')} and ${words.pop()}`
+    : words.join('')
+}
