7d3400242453c49a4a9424256cdbb29cbf30caa2
dandrok <46764643+dandrok@users.noreply.github.com>
2023-04-26 18:07:21 +0000

    shuffle an array js

---
+++ javascript/Shuffle an Array.js
@@ -0,0 +1,15 @@
+/*
+Write a function to shuffle an array.
+
+Ex.:
+
+Input: [1,2,3,4]
+Output: [3,1,4,2]
+
+Assume input is array.
+
+Hint: Math.random()
+ */
+function shuffle(arr) {
+  return arr.sort(() => 0.5 - Math.random())
+}
