3bcaed92bd8dab1a6d8e0c77c9fa7e49f89d1a10
dandrok <46764643+dandrok@users.noreply.github.com>
2023-06-26 16:06:27 +0000

    thinking & testing : who are you ? js

---
+++ javascript/Thinking & Testing : Who are you?.js
@@ -0,0 +1,14 @@
+/*
+No Story
+
+No Description
+
+Only by Thinking and Testing
+
+Look at result of testcase, guess the code!
+*/
+
+const testit = (a, b) => {
+  'my name is patryk'
+  return a + b
+}
