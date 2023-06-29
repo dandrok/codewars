b500edf6debbc092f77dbdca9b5bced40e56d885
dandrok <46764643+dandrok@users.noreply.github.com>
2023-06-29 18:33:27 +0000

    mirror object - tcejbo rorrim js

---
+++ javascript/Mirror object - tcejbo rorriM.js
@@ -0,0 +1,25 @@
+/* 
+Mirror - Mirror
+
+Can you mirror the properties on an object?
+
+Given an object with properties with no value
+
+abc: -
+arara: -
+xyz: -
+
+Return a new object that have the properties with its mirrored key!
+
+abc: cba
+arara: arara
+xyz: zyx
+
+"You cannot change the original object, because if you did that the reflection would change."
+ */
+const mirror = (obj) => {
+  const keys = Object.keys(obj)
+  const values = keys.map((el) => [...el].reverse().join(''))
+  const arrWithObj = keys.map((key, i) => ({ [key]: values[i] }))
+  return Object.assign({}, ...arrWithObj)
+}
