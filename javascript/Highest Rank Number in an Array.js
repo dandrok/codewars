e0ad44f3aa4e11fcfd48933fec9db063d1a684ad
dandrok <46764643+dandrok@users.noreply.github.com>
2023-07-28 05:21:00 +0000

    highest rank number in an array js

---
+++ javascript/Highest Rank Number in an Array.js
@@ -0,0 +1,24 @@
+/*
+Complete the method which returns the number which is most frequent in the given input array.
+If there is a tie for most frequent number, return the largest number among them.
+
+Note: no empty arrays will be given.
+Examples
+
+[12, 10, 8, 12, 7, 6, 4, 10, 12]              -->  12
+[12, 10, 8, 12, 7, 6, 4, 10, 12, 10]          -->  12
+[12, 10, 8, 8, 3, 3, 3, 3, 2, 4, 10, 12, 10]  -->   3
+ */
+function highestRank(arr) {
+  const counts = arr.reduce(
+    (acc, value) => ({
+      ...acc,
+      [value]: (acc[value] || 0) + 1,
+    }),
+    {}
+  )
+
+  return Number(
+    Object.keys(counts).reduce((a, b) => (counts[a] > counts[b] ? a : b))
+  )
+}
