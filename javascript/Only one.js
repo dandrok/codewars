ad4de89b5ad521e1becc288d714cf1cd652452c3
dandrok <46764643+dandrok@users.noreply.github.com>
2023-11-08 09:39:20 +0000

    feat: only one js

---
+++ javascript/Only one.js
@@ -0,0 +1,11 @@
+/*
+Given any number of boolean flags function should return true if and only if one of them is true while others are false. If function is called without arguments it should return false.
+
+  onlyOne() --> false
+  onlyOne(true, false, false) --> true
+  onlyOne(true, false, false, true) --> false
+  onlyOne(false, false, false, false) --> false  
+ */
+function onlyOne(...params) {
+  return params.filter(Boolean).length === 1
+}
