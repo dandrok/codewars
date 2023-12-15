7c12c4877b1dcf7141c8ed60e15c5f4339e1a2fb
dandrok <46764643+dandrok@users.noreply.github.com>
2023-12-15 18:46:36 +0000

    feat: proof read js

---
+++ javascript/Proof Read.js
@@ -0,0 +1,31 @@
+/* 
+You've just finished writing the last chapter for your novel when a virus suddenly infects your document. It has swapped the 'i's and 'e's in 'ei' words and capitalised random letters. Write a function which will:
+
+a) remove the spelling errors in 'ei' words. (Example of 'ei' words: their, caffeine, deceive, weight)
+
+b) only capitalise the first letter of each sentence. Make sure the rest of the sentence is in lower case.
+
+Example: He haD iEght ShOTs of CAffIEne. --> He had eight shots of caffeine.
+ */
+function proofread(str) {
+  str = str.toLowerCase()
+
+  const fixSpelling = str
+    .split(' ')
+    .map((word) => word.replace('ie', 'ei'))
+    .join(' ')
+
+  return fixSpelling
+    .split('. ')
+    .map((sentense) => `${sentense.at().toUpperCase()}${sentense.slice(1)}`)
+    .join('. ')
+}
+
+function proofread(str) {
+  return str
+    .toLowerCase()
+    .replaceAll('ie', 'ei')
+    .split('. ')
+    .map((sentense) => `${sentense.at().toUpperCase()}${sentense.slice(1)}`)
+    .join('. ')
+}
