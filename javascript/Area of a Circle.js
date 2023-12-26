501600a6f2032b72461164fb375e670d7be52d4f
dandrok <46764643+dandrok@users.noreply.github.com>
2023-12-26 09:54:28 +0000

    feat: area of a circle js

---
+++ javascript/Area of a Circle.js
@@ -0,0 +1,18 @@
+/* 
+Returned value is expected to be accurate up to tolerance of 0.01.
+
+If the radius is not positive, throw Error.
+
+Example:
+
+circleArea(43.2673);     // returns 5881.248  (± 0.01)
+circleArea(68);          // returns 14526.724 (± 0.01)
+circleArea(0);           // throws Error
+circleArea(-1);          // throws Error
+ */
+function circleArea(radius) {
+  if (radius < 1) {
+    throw new Error('err')
+  }
+  return Math.PI * radius ** 2
+}
