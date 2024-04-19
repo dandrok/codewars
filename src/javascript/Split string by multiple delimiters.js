25a387f43bf1e6515132c678f831148f6ea3a965
dandrok <patryk7511@o2.pl>
2024-04-19 19:59:38 +0200

    feat: split string by multiple delimiters js

---
+++ src/javascript/Split string by multiple delimiters.js
@@ -0,0 +1,18 @@
+/* 
+Your task is to write function which takes string and list of delimiters as an input and returns list of strings/characters after splitting given string.
+
+Example:
+
+multipleSplit('Hi, how are you?', [' ']) => ['Hi,', 'how', 'are', 'you?']
+multipleSplit('1+2-3', ['+', '-']) => ['1', '2', '3']
+List of delimiters is optional and can be empty, so take that into account.
+
+Important note: Result cannot contain empty string.
+
+*/
+
+function multipleSplit(string, delimiters = []) {
+  const regExp = new RegExp(`[${delimiters.join("\\")}]`, "g");
+  const replaced = string.replace(regExp, "§");
+  return replaced.split("§").filter((el) => el.length);
+}
