7b40322bfb9ce20728425cf586fbca8a14788a6c
dandrok <46764643+dandrok@users.noreply.github.com>
2024-04-29 13:01:00 +0200

    New test for test GitHub mobile commits

---
+++ src/tests/fromMobile.test.js
@@ -0,0 +1,8 @@
+import {describe, it, expect} from "vitest"
+
+describe("add test", () => {
+   it("should return two", () => {
+    const count = 1 + 1
+    expect(count).toStrictEqual(2)
+   }) 
+})
