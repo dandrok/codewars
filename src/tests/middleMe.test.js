189719f5ee0865a572f3c8f51cbde6f0bdbc70e0
dandrok <46764643+dandrok@users.noreply.github.com>
2024-05-06 05:02:49 +0200

    Update middleMe.test.js

--- src/tests/middleMe.test.js
+++ src/tests/middleMe.test.js
@@ -3,17 +3,19 @@ import {describe, it, expect } from "virest"
 describe('testing', () => {
     
     it("should be true", () => {
-    const testingWord = "aaaBaaa"
-    const n = 6
-    const x = "B"
-    const y = "a"
+        const testingWord = "aaaBaaa"
+        const n = 6
+        const x = "B"
+        const y = "a"
     
-    expect(middleMe(n, x, y)).toBe(testingWord)
+        expect(middleMe(n, x, y)).toBe(testingWord)
     })
+    
     it("should be x", () => {
-    const n = 7
-    const x = "B"
-    const y = "a"
-    expect(middleMe(n, x, y)).toBe(x)
+        const n = 7
+        const x = "B"
+        const y = "a"
+        
+        expect(middleMe(n, x, y)).toBe(x)
     })
 })
