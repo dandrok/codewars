da84cce0e5abcadc916ccdbed87bbc18d07c20d4
dandrok <patryk7511@o2.pl>
2024-05-17 18:55:37 +0200

    feat: guess the sequence js

---
+++ src/javascript/Guess the Sequence.js
@@ -0,0 +1,18 @@
+/* 
+You have read the title: you must guess a sequence. It will have something to do with the number given.
+Example
+
+x = 16
+
+result = [1, 10, 11, 12, 13, 14, 15, 16, 2, 3, 4, 5, 6, 7, 8, 9]
+
+Good luck!
+*/
+
+function sequence(x) {
+  let arr = [];
+  for (let i = 1; i <= x; i++) {
+    arr.push(i);
+  }
+  return arr.sort();
+}
