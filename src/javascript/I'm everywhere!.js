a44c97fe08face8e9d39f7d07514ea455c52b961
dandrok <46764643+dandrok@users.noreply.github.com>
2024-06-23 17:09:45 +0000

    feat: i m everywhere! js

---
+++ src/javascript/I'm everywhere!.js
@@ -0,0 +1,28 @@
+/* 
+Overview
+
+Many people know that Apple uses the letter "i" in almost all of its devices to emphasize its personality.
+
+And so John, a programmer at Apple, was given the task of making a program that would add that letter to every word. Let's help him do it, too.
+Task:
+
+Your task is to make a function that takes the value of word and returns it with an "i" at the beginning of the word. For example we get the word "Phone", so we must return "iPhone". But we have a few rules:
+
+    The word should not begin with the letter "I", for example Inspire.
+    The number of vowels should not be greater than or equal to the number of consonants, for example East or Peace. ("y" is considered a consonant)
+    The first letter should not be lowercase, for example road.
+
+If the word does not meet the rules, we return "Invalid word".
+*/
+
+function i(word) {
+  const consonants = word.match(/[bcdfghjklmnpqrstvwxyz]/gi)?.length || 0;
+  const vowels = word.match(/[aeiou]/gi)?.length || 0;
+  const firstLetterCheck =
+    (word && !(word[0] === word[0].toUpperCase())) || word[0] === "I";
+  if (firstLetterCheck || vowels >= consonants) {
+    return "Invalid word";
+  } else {
+    return `i${word}`;
+  }
+}
