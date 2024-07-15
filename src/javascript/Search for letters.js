eed78de59caadda6f3aa90708bc899e467a847b5
dandrok <46764643+dandrok@users.noreply.github.com>
2024-07-15 09:12:22 +0000

    feat: search for letters js

---
+++ src/javascript/Search for letters.js
@@ -0,0 +1,58 @@
+/* 
+Create a function which accepts one arbitrary string as an argument, and return a string of length 26.
+
+The objective is to set each of the 26 characters of the output string to either '1' or '0' based on the fact whether the Nth letter of the alphabet is present in the input (independent of its case).
+
+So if an 'a' or an 'A' appears anywhere in the input string (any number of times), set the first character of the output string to '1', otherwise to '0'. if 'b' or 'B' appears in the string, set the second character to '1', and so on for the rest of the alphabet.
+
+For instance:
+
+"a   **&  cZ"  =>  "10100000000000000000000001"
+
+*/
+
+function change(s) {
+  if (!isNaN(s)) {
+    return objToStr(alphabet);
+  }
+  s = s.toLowerCase();
+  const lettersArr = s.match(/[a-z]/gi);
+  const obj = lettersArr.reduce(
+    (acc, curr) => {
+      acc[curr] = 1 || 0;
+      return acc;
+    },
+    { ...alphabet }
+  );
+  return objToStr(obj);
+}
+
+const objToStr = (object) => Object.values(object).join("");
+const alphabet = {
+  a: 0,
+  b: 0,
+  c: 0,
+  d: 0,
+  e: 0,
+  f: 0,
+  g: 0,
+  h: 0,
+  i: 0,
+  j: 0,
+  k: 0,
+  l: 0,
+  m: 0,
+  n: 0,
+  o: 0,
+  p: 0,
+  q: 0,
+  r: 0,
+  s: 0,
+  t: 0,
+  u: 0,
+  v: 0,
+  w: 0,
+  x: 0,
+  y: 0,
+  z: 0,
+};
