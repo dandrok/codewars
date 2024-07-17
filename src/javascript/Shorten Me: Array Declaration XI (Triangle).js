ec0ac273e53eee84cf5d4903ad34a4ef368bb795
dandrok <46764643+dandrok@users.noreply.github.com>
2024-07-17 17:33:57 +0000

    feat: shorten me array deciaration xi trianagle js

---
+++ src/javascript/Shorten Me: Array Declaration XI (Triangle).js
@@ -0,0 +1,26 @@
+/* 
+Shorten Me: Array Declaration XI (Triangle)
+Task
+
+You have to write an array() function returning an array object with elements as follow:
+
+[
+    [9,8,7,6,5,4,3,2,1,0],
+    [8,7,6,5,4,3,2,1,0],
+    [7,6,5,4,3,2,1,0],
+    [6,5,4,3,2,1,0],
+    [5,4,3,2,1,0],
+    [4,3,2,1,0],
+    [3,2,1,0],
+    [2,1,0],
+    [1,0],
+    [0]
+]
+
+Coding Limitation:
+
+Less than 70 characters
+*/
+
+// result
+// array=_=>(x=>x.map((_,i)=>x.slice(i)))([9,8,7,6,5,4,3,2,1,0]);
