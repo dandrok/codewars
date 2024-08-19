190765eb667d6d2e7fad297bfe91439cd0cf1645
dandrok <46764643+dandrok@users.noreply.github.com>
2024-08-19 15:06:54 +0000

    feat: some but not all js

---
+++ src/javascript/Some (but not all).js
@@ -0,0 +1,18 @@
+/* 
+Some but not all
+Description
+
+Your task is to create a function that given a sequence and a predicate, returns True if only some (but not all) the elements in the sequence are True after applying the predicate
+Examples
+
+('abcdefg&%$', x -> isLetter(x)) == true
+('&%$=', x -> isLetter x) == false
+('abcdefg', x -> isLetter x) == false
+
+([4, 1], x -> x > 3) == true
+([1, 1], x -> x > 3) == false
+([4, 4], x -> x > 3) == false
+
+*/
+
+const someButNotAll = ([...seq], pred) => seq.some(pred) && !seq.every(pred);
