21f2964015561abbc824232389375728feda6edd
dandrok <46764643+dandrok@users.noreply.github.com>
2024-09-13 17:14:48 +0000

    feat: sentence to words js

---
+++ src/javascript/Sentence to words.js
@@ -0,0 +1,9 @@
+/* 
+Write function splitSentence which will create a list of strings from a string.
+
+Example:
+
+"hello world" -> ["hello", "world"]
+*/
+
+const splitSentence = (s) => s.split(" ");
