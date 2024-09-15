dcdd3965966187822ebf6ec106099e1aa38a0312
dandrok <46764643+dandrok@users.noreply.github.com>
2024-09-15 15:46:17 +0000

    feat: identical elements js

---
+++ src/javascript/Identical Elements.js
@@ -0,0 +1,7 @@
+/* 
+Given two arrays of integers m and n, test if they contain at least one identical element. Return true if they do; false if not.
+
+Your code must handle any value within the range of a 32-bit integer, and must be capable of handling either array being empty (which is a false result, as there are no duplicated elements).
+*/
+
+const duplicateElements = (m, n) => m.some((el) => n.includes(el));
