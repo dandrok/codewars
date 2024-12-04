388325a8b26cae3422d2ffa35e77e6ef5dc1643c
dandrok <46764643+dandrok@users.noreply.github.com>
2024-12-04 18:47:59 +0000

    feat: shorten me - mad mathematicians algorithm addition js

---
+++ src/javascript/Shorten Me: Mad Mathematician's Algorithm I (Addition).js
@@ -0,0 +1,12 @@
+/* 
+Task
+
+You have to write a madAdd(a,b) function returning a value of a + b without + in your code.
+Coding Limitation:
+
+    Less than 30 characters
+
+Input:
+
+    a and b : integer ( 0-10000 )
+*/
