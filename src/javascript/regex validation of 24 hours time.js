b1b73e85a4ed5c3e5c11d8e4a20508d5941bc9b6
dandrok <46764643+dandrok@users.noreply.github.com>
2025-03-15 17:54:15 +0000

    feat: regexp validation of 24 hours time js

---
+++ src/javascript/regex validation of 24 hours time.js
@@ -0,0 +1,14 @@
+/* 
+Write a regex to validate a 24 hours time string. See examples to figure out what you should check for:
+
+Accepted: 01:00 - 1:00
+
+Not accepted:
+
+24:00
+
+You should check for correct length and no spaces.
+*/
+
+const validateTime = (time) =>
+  /^(0?[0-9]|1[0-9]|2[0-3]):(0?[0-5][0-9])$/.test(time);
