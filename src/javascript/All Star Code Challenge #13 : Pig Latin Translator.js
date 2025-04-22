a19d2277b0f1f5c891a53bc8908c0620c5bb9dfb
dandrok <46764643+dandrok@users.noreply.github.com>
2025-04-22 16:10:09 +0000

    feat: all star code challenge #13 pig latin trnslator js

---
+++ src/javascript/All Star Code Challenge #13 : Pig Latin Translator.js
@@ -0,0 +1,31 @@
+/* 
+This Kata is intended as a small challenge for my students
+
+All Star Code Challenge #13
+
+Your friend Billybob has a crush on the girl next to him in class, Emily, but wants to talk with you about what he should do, but doesn't want her to overhear. Send secret messages to Billybob by translating your messages into pig latin.
+
+Create a function, called translate(), that takes a string argument and returns the input string into "pig latin"
+
+The rules to translate into pig latin are as follows:
+
+Valid words are 2 or more letters long
+If a word begins with a consonant (a letter NOT 'a','e','i','o', or 'u'), then that first letter is shifted to the end of the word
+Then add "ay"
+translate("billy"); // => "illybay"
+translate("emily"); // => "emilyay"
+Note:
+If the string input is less than 2 letters, the input should be returned as is
+
+*/
+
+const translate = (word) => {
+  if (word.length < 2) {
+    return word;
+  }
+  const firstChar = word.at(0);
+  const newWorld = /^[^aeiou]/.test(firstChar)
+    ? word.slice(1) + firstChar
+    : word;
+  return `${newWorld}ay`;
+};
