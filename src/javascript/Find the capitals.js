5acb93f9b6e1f9f92bc9d133b22360fdac9cfc4b
dandrok <patryk7511@o2.pl>
2025-07-10 19:37:47 +0200

    feat(): volume of a cuboid

--- src/javascript/Find the capitals.js
+++ src/javascript/Find the capitals.js
@@ -1,15 +1,22 @@
 /* 
-Instructions
-Write a function that takes a single string (word) as argument. The function must return an ordered list containing the indexes of all capital letters in the string.
+Complete the method that takes a sequence of objects with two keys each: country or state, and capital. Keys may be symbols or strings.
 
-Example
-Test.assertSimilar( capitals('CodEWaRs'), [0,3,4,6] );
- */
+The method should return an array of sentences declaring the state or country and its capital.
+Examples
 
-const capitals = (word) => {
-  return [...word]
-    .map((el, i) => (el === el.toUpperCase() ? i : ''))
-    .join('')
-    .split('')
-    .map(Number)
-}
+state_capitals = [{state: 'Maine', capital: 'Augusta'}]
+capital(state_capitals)[0] // returns "The capital of Maine is Augusta"
+
+country_capitals = [{'country' : 'Spain', 'capital' : 'Madrid'}]
+capital(country_capitals)[0]  // returns "The capital of Spain is Madrid"
+
+mixed_capitals: [{"state" : 'Maine', capital: 'Augusta'}, {country: 'Spain', "capital" : "Madrid"}]
+capital(mixed_capitals)[1] // returns "The capital of Spain is Madrid"
+
+*/
+
+const capital = (capitals) =>
+  capitals.map(
+    ({ state, capital, country }) =>
+      `The capital of ${state || country} is ${capital}`
+  );
