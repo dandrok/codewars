35dbd4839263fb87fcd13bc0c916c218fad04f60
dandrok <46764643+dandrok@users.noreply.github.com>
2025-07-10 17:42:37 +0000

    feat: volume of a cuboid ts

--- src/typescript/Volume of a Cuboid.ts
+++ src/typescript/Volume of a Cuboid.ts
@@ -1,7 +1,9 @@
-/* 
+/*
+
 Bob needs a fast way to calculate the volume of a rectangular cuboid with three values: the length, width and height of the cuboid.
 
 Write a function to help Bob with this calculation.
+
 */
 
 export function getVolumeOfCuboid(
