abffe9b4636e67118abbb4cdf43cec93016fdd36
dandrok <patryk.stusik@gmail.com>
2025-08-23 18:59:44 +0200

    feat(): draw stairs ts

---
+++ src/typescript/Draw stairs.ts
@@ -0,0 +1,29 @@
+/* 
+Given a number n, draw stairs using the letter "I", n tall and n wide, with the tallest in the top left.
+
+For example n = 3 result in:
+
+"I\n I\n  I"
+or printed:
+
+I
+ I
+  I
+Another example, a 7-step stairs should be drawn like this:
+
+I
+ I
+  I
+   I
+    I
+     I
+      I 
+*/
+
+export function drawStairs(n: number): string {
+  let result: string[] = [];
+  for (let i = 0; i < n; i++) {
+    result.push(`${" ".repeat(i)}I`);
+  }
+  return result.join("\n");
+}
