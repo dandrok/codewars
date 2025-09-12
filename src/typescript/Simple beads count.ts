98c69761dd4a5c073ceef0015886d728b55ab95b
dandrok <patryk.stusik@gmail.com>
2025-09-12 19:24:07 +0200

    feat(): simple beads count ts

---
+++ src/typescript/Simple beads count.ts
@@ -0,0 +1,12 @@
+/* 
+Two red beads are placed between every two blue beads. There are N blue beads. After looking at the arrangement below work out the number of red beads.
+
+@ @@ @ @@ @ @@ @ @@ @ @@ @
+
+Implement a function returning the number of red beads.
+If there are less than 2 blue beads return 0.
+*/
+
+export function countRedBeads(n: number): number {
+  return Math.max(n - 1, 0) * 2;
+}
