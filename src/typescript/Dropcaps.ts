14299924787390ddccc8e68374667326a912843f
dandrok <patryk.stusik@gmail.com>
2025-09-13 18:57:55 +0200

    feat(): dropcaps ts

---
+++ src/typescript/Dropcaps.ts
@@ -0,0 +1,26 @@
+/* 
+DropCaps means that the first letter of the starting word of the paragraph should be in caps and the remaining lowercase, just like you see in the newspaper.
+
+But for a change, let"s do that for each and every word of the given String. Your task is to capitalize every word that has length greater than 2, leaving smaller words as they are.
+
+*should work also on Leading and Trailing Spaces and caps.
+
+"apple"            => "Apple"
+"apple of banana"  => "Apple of Banana"
+"one   space"      => "One   Space"
+"   space WALK   " => "   Space Walk   " 
+Note: you will be provided atleast one word and should take string as input and return string as output.
+*/
+
+export const dropCap = (s: string): string =>
+  s
+    .split(" ")
+    .map((el) => {
+      if (el.length > 2) {
+        const word = el.toLowerCase();
+        return word.at(0)?.toUpperCase() + word.slice(1);
+      } else {
+        return el;
+      }
+    })
+    .join(" ");
