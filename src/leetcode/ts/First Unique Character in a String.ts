aeabd3e963b686ffe09390f2cdcb698159c8ba27
dandrok <patryk.stusik@gmail.com>
2025-11-18 20:52:00 +0100

    first unique character in a string ts

---
+++ src/leetcode/ts/First Unique Character in a String.ts
@@ -0,0 +1,48 @@
+/*
+Giiven a string s, find the first non-repeating character in it and return its index. If it does not exist, return -1.
+
+ 
+
+Example 1:
+
+Input: s = "leetcode"
+
+Output: 0
+
+Explanation:
+
+The character 'l' at index 0 is the first character that does not occur at any other index.
+
+Example 2:
+
+Input: s = "loveleetcode"
+
+Output: 2
+
+Example 3:
+
+Input: s = "aabb"
+
+Output: -1
+
+ 
+
+Constraints:
+
+    1 <= s.length <= 105
+    s consists of only lowercase English letters.
+
+ 
+*/
+function firstUniqChar(s: string): number {
+  const letters = [...s]
+  let uresult = []
+  for (const letter of letters) {
+    console.log(letter)
+    if (letters.filter(l => l === letter).length <= 1) {
+      result.push(letter)
+    }
+  }
+  return s.indexOf(result[0])
+};
+
