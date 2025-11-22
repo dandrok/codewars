70718c0d363f0735c2f3f3a3a0c5c6111ef925fd
dandrok <patryk.stusik@gmail.com>
2025-11-22 21:01:45 +0100

    odds index js

---
+++ src/javascript/Odds-Index.js
@@ -0,0 +1,10 @@
+/* 
+*
+You are given an array with several "even" words, one "odd" word, and some numbers mixed in.
+
+Determine if any of the numbers in the array is the index of the "odd" word. If so, return true, otherwise false.
+*/
+function oddBall(arr) {
+  const indexOfOdd = arr.indexOf('odd')
+  return arr.some(num => num === indexOfOdd)
+}
