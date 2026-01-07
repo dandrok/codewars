f053e219f91ea49d088a212fa3a60fd9cc32d5c9
dandrok <patryk.stusik@gmail.com>
2026-01-07 20:10:13 +0100

    refactor: a promise is a promise js

--- src/javascript/A Promise is a Promise.js
+++ src/javascript/A Promise is a Promise.js
@@ -3,3 +3,7 @@ Create a resolved javascript Promise that will return 'Hello World!'.
  */
 
 const promiseHelloWorld = async () => "Hello World!";
+
+const promiseHelloWorld = () => new Promise(resolve => resolve("Hello World!"))
+
+const promiseHelloWorld = () => Promise.resolve("Hello World!")
