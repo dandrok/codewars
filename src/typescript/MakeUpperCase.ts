2c7e674d67cd3c392e1e9a5ecfb03d88c37f6313
dandrok <patryk.stusik@gmail.com>
2026-02-21 21:26:51 +0100

    refactor: make upper case ts

--- src/typescript/MakeUpperCase.ts
+++ src/typescript/MakeUpperCase.ts
@@ -5,3 +5,6 @@ Write a function which converts the input string to uppercase.
 export function makeUpperCase(str: string): string {
   return str.toUpperCase();
 }
+
+
+export const makeUpperCase = (s: string) => s.toUpperCase()
