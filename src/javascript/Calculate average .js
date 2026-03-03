8f73b9a7e7f296bafdbc1627913ea992ac18324b
dandrok <patryk.stusik@gmail.com>
2026-03-03 19:35:19 +0100

    refactor: calculate average js

--- src/javascript/Calculate average .js
+++ src/javascript/Calculate average .js
@@ -3,7 +3,7 @@ Write a function which calculates the average of the numbers in a given list.
 
 Note: Empty arrays should return 0.
  */
-function findAverage(array) {
+const findAverage = (array) => {
   if (!array.length) return 0
   return array.reduce((a, b) => a + b, 0) / array.length
 }
