ad0c08273b3d9e0d76d467ab88536ee064ae34c8
dandrok <patryk.stusik@gmail.com>
2026-03-10 21:07:07 +0100

    refactor: who took the car key js

--- src/javascript/Who Took The Car Key?.js
+++ src/javascript/Who Took The Car Key?.js
@@ -8,8 +8,8 @@ Given an array of binary numbers, figure out and return the culprit's message to
 > whoTookTheCarKey(['01000001', '01101100', '01100101', '01111000', '01100001', '01101110', '01100100', '01100101', '01110010'])
 'Alexander'
  */
-function whoTookTheCarKey(message) {
-  return message
+const whoTookTheCarKey = (message) =>
+  message
     .map((el) => String.fromCharCode(parseInt(String(el), 2)))
     .join('')
-}
+
