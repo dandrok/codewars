e6277db61a21340aa1e46a93b12cd42bea7c82f8
dandrok <patryk.stusik@gmail.com>
2026-03-14 19:49:04 +0100

    refactor: string templates bug fixing 5 ts

--- src/typescript/String Templates - Bug Fixing #5.ts
+++ src/typescript/String Templates - Bug Fixing #5.ts
@@ -2,5 +2,4 @@
 Oh no! Timmy hasn't followed instructions very carefully and forgot how to use the new String Template feature, Help Timmy with his string template so it works as he expects!
  */
 
-export const buildString = (...template: string[]): string =>
-  `I like ${template.join(', ')}!`
+export const buildString = (...template: string[]): string => `I like ${template.join(', ')}!`
