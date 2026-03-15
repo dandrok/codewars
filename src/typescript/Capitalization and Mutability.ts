29ee9295116ea54d34ea3da907dd2450de77a871
dandrok <patryk.stusik@gmail.com>
2026-03-15 19:52:34 +0100

    refactor: capitalization and mutability ts

--- src/typescript/Capitalization and Mutability.ts
+++ src/typescript/Capitalization and Mutability.ts
@@ -12,6 +12,4 @@ Examples:
 "a"     --> "A"
 */
 
-export function capitalizeWord(word: string): string {
-  return `${word[0].toUpperCase()}${word.slice(1)}`;
-}
+export const capitalizeWord = (word: string): string => `${word[0].toUpperCase()}${word.slice(1)}`;
