74a5ee4c38d00efa4a751cf517785e83ed465d47
dandrok <patryk.stusik@gmail.com>
2026-03-22 14:18:07 +0100

    feat: implement safe length comparison utility js

---
+++ src/kumites/ImplementSafeLengthComparisonUtility.js
@@ -0,0 +1,9 @@
+// created sumletters(a, b) to return true if the lengths of both inputs match.
+//
+// returns false when eaither argument is null or undefined.
+
+
+const sumLetters = (a, b) => a?.length === b?.length
+
+
+
