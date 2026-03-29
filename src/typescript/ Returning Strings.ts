a6262213ac0c691555788e7fad22f4dc55a9714c
dandrok <patryk.stusik@gmail.com>
2026-03-29 20:27:08 +0200

    refactor: return strings ts

--- src/typescript/ Returning Strings.ts
+++ src/typescript/ Returning Strings.ts
@@ -4,5 +4,4 @@ Make a function that will return a greeting statement that uses an input; your p
 [Make sure you type the exact thing I wrote or the program may not execute properly]
  */
 
-export const greet = (name: string): string =>
-  `Hello, ${name} how are you doing today?`
+export const greet = (name: string): string => `Hello, ${name} how are you doing today?`
