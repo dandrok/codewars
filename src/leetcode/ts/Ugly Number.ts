1796d3e52ea97e747b2fb4cc00654c5d6d8ef6cb
dandrok <patryk.stusik@gmail.com>
2026-08-25 11:35:40 +0200

    feat: ugly number ts - solution

--- src/leetcode/ts/Ugly Number.ts
+++ src/leetcode/ts/Ugly Number.ts
@@ -29,5 +29,26 @@
 //     -231 <= n <= 231 - 1
 
 function isUgly(n: number): boolean {
-    
-};
+  if (n <= 0) return false;
+  if (n  === 1) return true; 
+  
+  const primes = [2, 3, 5];
+  for (let i = 0; i < primes.length; i++) {
+    while (n % primes[i] === 0) {
+      n = n / primes[i];
+      if (n === 1) return true;
+    }
+  }
+  return false;
+}
+// time compelxity is equal O(log n)
+// the for loop time complexity is equal to O(1) 
+// becouse we have set of three values in the array
+// and the while loop time complexity is O(log n),
+// becouse we divide our value by 2 | 3 | 5
+// so it is way faster than regular O(n)
+// ---
+// space complexity is pretty quick O(1)
+// becouse we simply set new n in place so
+// we dont do any expensive operations
+// simply we ovverrite one value in place.
