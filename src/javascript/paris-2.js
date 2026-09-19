e6e6c4370aeda4682a3641e1d9b750b80e3f6d95
dandrok <patryk.stusik@gmail.com>
2026-09-19 19:21:45 +0200

    refactor: math pow method

--- src/javascript/paris-2.js
+++ src/javascript/paris-2.js
@@ -5,4 +5,8 @@
 Now you have to write a function that takes an argument and returns the square of it.
 */
 // Write the "square"-function here
-const square =(num)=>num*num
+const square = (num) => num * num;
+
+//or
+const square = (num) => Math.pow(num, 2);
+
